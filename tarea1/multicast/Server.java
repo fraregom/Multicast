66e9746d2d9474b941d685a83b35030f34a35b74
fraregom <servoclay@gmail.com>
2018-10-14 22:12:39 -0300

    funciona el cliente/servidor

--- tarea1/multicast/Server.java
+++ tarea1/multicast/Server.java
@@ -1,15 +1,26 @@
 package tarea1.multicast;
 
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Server {
+
 
     public static void main(String [] args){
 
         try {
-            ServerSocket serverSocket = new ServerSocket(9999);
+            ServerSocket server = new ServerSocket(9999);
+
+            Socket socket = server.accept();
+
+            DataInputStream stream = new DataInputStream(socket.getInputStream());
+
+            String message = stream.readUTF();
+
+            System.out.println(message);
+
+            socket.close();
 
         } catch (IOException e) {
 
