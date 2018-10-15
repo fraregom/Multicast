b0ae250b678932ecd6b008a9132678c1eeea5025
Francisco Reyes <servoclay@gmail.com>
2018-10-14 21:36:05 -0300

    Trabajando en sockets

--- tarea1/multicast/Client.java
+++ tarea1/multicast/Client.java
@@ -2,23 +2,21 @@ package tarea1.multicast;
 
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.*;
 
 public class Client {
 
     public static void main(String [] args){
 
         try {
-            Socket socket = new Socket("127.0.0.1", 9999);
+            Socket socket = new Socket("192.168.0.5", 9999); //Localhost 127.0.0.1
 
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-            
-
+            inputStream.writeUTF("Hola mundo!");
+            inputStream.close();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
 
     }
 
