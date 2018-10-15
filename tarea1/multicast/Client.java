e430288f6a42a2ffd36f4b1b4984a888a7631ce6
fraregom <servoclay@gmail.com>
2018-10-14 23:51:22 -0300

    Es posible enviar peticiones al servidor!

--- tarea1/multicast/Client.java
+++ tarea1/multicast/Client.java
@@ -6,9 +6,12 @@ import java.net.*;
 
 public class Client {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
-        try {
+        clientFrame client = new clientFrame();
+
+
+        /*try {
             Socket socket = new Socket("127.0.1", 9999); //Localhost 127.0.0.1
 
             DataOutputStream stream = new DataOutputStream(socket.getOutputStream());
@@ -18,8 +21,40 @@ public class Client {
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }*/
+    }
+}
+
+class clientFrame{
+    int port;
+    InetAddress address;
+    DatagramSocket socket = null;
+    DatagramPacket packet;
+    byte[] sendBuf = new byte[256];
+
+    public clientFrame(){
+        try {
+            socket = new DatagramSocket(); //Se crea un socket para enviar el paquete a crear.
+
+            byte[] buf = new byte[256]; //Se definen los datos de donde el cliente envio la peticion.
+            InetAddress address = InetAddress.getLocalHost();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445); //se define que la peticion se enviara al puerto 4445 del server
+            socket.send(packet);
+
+
+            packet = new DatagramPacket(buf, buf.length); //Esto imprimira lo que responda el servidor.
+            socket.receive(packet);
+            String received = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Quote of the Moment: " + received);
+
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
     }
 
 }
+
+
+
--- tarea1/multicast/Server.java
+++ tarea1/multicast/Server.java
@@ -1,13 +1,17 @@
 package tarea1.multicast;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.*;
 
-public class Server {
+public class Server{
 
+    public static void main(String[] args) throws IOException {
+        ThreadServer server = new ThreadServer();
+    }
 
-    public static void main(String [] args){
+    /*public static void main(String [] args){
 
         try {
             ServerSocket server = new ServerSocket(9999);
@@ -27,6 +31,56 @@ public class Server {
             System.out.println(e.getMessage());
         }
 
+
+    }*/
+
+}
+
+
+class ThreadServer implements Runnable{
+
+    public ThreadServer(){
+        Thread thread = new Thread(this); //Inicializa un thread para realizar multiples funciones.
+        thread.start();
+    }
+
+
+    @Override
+    public void run() {
+
+        try{
+            BufferedReader input = null;
+            String dString = null;
+            DatagramSocket socket = new DatagramSocket(4445); //Se define un socket a la escucha de peticiones en el puerto 4445.
+            input = new BufferedReader(new FileReader("/home/havok1177/Documents/Multicast/tarea1/multicast/one-liners.txt")); //cambiar esto!!!
+
+            while ((dString = input.readLine()) != null){
+
+                byte[] buf = new byte[256];
+                int port;
+
+                try {
+
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);  //Se obtiene toda la informacion del cliente para enviar una respuesta.
+                    socket.receive(packet);
+                    InetAddress address = packet.getAddress();
+                    port = packet.getPort();
+
+                    buf = dString.getBytes(); //Se convierte en bytes una linea del texto.
+                    packet = new DatagramPacket(buf, buf.length, address, port); //Se prepara el paquete a enviar al cliente.
+                    socket.send(packet);
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
